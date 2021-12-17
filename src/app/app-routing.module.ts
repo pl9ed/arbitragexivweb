f8d1eb10c612b44cdd90634a0d5d691f0c9b9293
Pai Li <pl9ed15@gmail.com>
2021-12-16 22:54:29 -0500

    Added routing for home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GemstoneComponent } from './pages/gemstone/gemstone.component';
 import { FlipComponent } from './pages/flip/flip.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
-  { path: 'gemstone', component: GemstoneComponent},
-  { path: '**', component: FlipComponent}
+  { path: 'flip', component: FlipComponent },
+  { path: 'gemstone', component: GemstoneComponent },
+  { path: '**', component: HomeComponent }
 ];
 
 @NgModule({
