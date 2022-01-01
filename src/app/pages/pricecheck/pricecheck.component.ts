e89fe8442c733687c4304d8dbbb553bfc129fb5e
Pai Li <pl9ed15@gmail.com>
2022-01-01 05:08:13 -0500

    Added basic add item functionality

--- src/app/pages/pricecheck/pricecheck.component.ts
+++ src/app/pages/pricecheck/pricecheck.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Item } from 'src/app/models/Item';
 import { SettingsService } from 'src/app/services/settings.service';
 import { UniversalisService } from 'src/app/services/universalis.service';
 
@@ -10,9 +11,40 @@ import { UniversalisService } from 'src/app/services/universalis.service';
 })
 export class PricecheckComponent implements OnInit {
 
+  static itemKey = 'PRICE_CHECK_ITEM'
+
+  items: Item[] = []
+
   constructor(private router: Router, private mbAPI: UniversalisService, private settings: SettingsService) { }
 
   ngOnInit(): void {
+    const items = localStorage.getItem(PricecheckComponent.itemKey)
+    console.log(items)
+    if (items) {
+      try {
+        this.items = JSON.parse(items)
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  }
+
+  addItem(name: string, id: number) {
+    if (name && id) {
+      const item: Item = { name, id }
+      this.items.push(item)
+      localStorage.setItem(PricecheckComponent.itemKey, JSON.stringify(this.items))
+      console.log(this.items)
+    }
+  }
+
+  removeItem(index: number) {
+    this.items.splice(index, 1)
+  }
+
+  clearItems() {
+    this.items = []
+    localStorage.setItem(PricecheckComponent.itemKey, JSON.stringify(this.items))
   }
 
 }
