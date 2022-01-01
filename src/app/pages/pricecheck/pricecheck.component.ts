e9e5252201e8a55bf5ab5ed89d1ef4bba28e5e24
Pai Li <pl9ed15@gmail.com>
2022-01-01 06:02:45 -0500

    Added prices + table

--- src/app/pages/pricecheck/pricecheck.component.ts
+++ src/app/pages/pricecheck/pricecheck.component.ts
@@ -15,8 +15,16 @@ export class PricecheckComponent implements OnInit {
   static itemKey = 'PRICE_CHECK_ITEM'
 
   items: Item[] = []
+  pricesNQ: number[] = []
+  pricesHQ: number[] = []
 
-  constructor(private router: Router, private mbAPI: UniversalisService, private settings: SettingsService) {}
+  headers = [
+    "Item",
+    "Price (NQ)",
+    "Price (HQ)"
+  ]
+
+  constructor(private router: Router, private mbAPI: UniversalisService, private settings: SettingsService) { }
 
   ngOnInit(): void {
     const itemsString = localStorage.getItem(PricecheckComponent.itemKey)
@@ -28,6 +36,8 @@ export class PricecheckComponent implements OnInit {
     } catch (e) {
       console.log(e)
     }
+
+    this.populatePrices()
   }
 
   addItem(name: string, idString: string) {
@@ -52,6 +62,16 @@ export class PricecheckComponent implements OnInit {
   clearItems() {
     this.items = []
     localStorage.setItem(PricecheckComponent.itemKey, JSON.stringify(this.items))
+  }
+
+  async populatePrices() {
+    for (let i = 0; i < this.items.length; i++) {
+      let item = this.items[i]
+      let prices = await this.mbAPI.getItem(this.settings.homeworld, item.id)
+      this.pricesNQ[i] = prices.minPriceNQ
+      this.pricesHQ[i] = prices.minPriceHQ
+      await this.mbAPI.sleep(500)
+    }
   }
 
 }
