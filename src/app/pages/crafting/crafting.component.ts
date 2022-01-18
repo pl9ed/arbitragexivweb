65f5ec658bd94abd440cafa1fd4059a70023cae3
Pai Li <pl9ed15@gmail.com>
2022-01-17 20:19:12 -0500

    Added method stub for getData

--- src/app/pages/crafting/crafting.component.ts
+++ src/app/pages/crafting/crafting.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Recipe } from 'src/app/models/Recipe';
+import { SettingsService } from 'src/app/services/settings.service';
+import { UniversalisService } from 'src/app/services/universalis.service';
+import { XivAPIService } from 'src/app/services/xiv-api.service';
 
 @Component({
   selector: 'app-crafting',
@@ -7,9 +12,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CraftingComponent implements OnInit {
 
-  constructor() { }
+  /**
+   * 2 arrays/tables? 
+   * ITEM NAME | MAT TOTAL | ROI
+   * VALUE | VALUE | VALUE
+   * 
+   * MAT1 | COST | WORLD
+   * MAT2 | COST | WORLD
+   * ...
+   * MATN | COST | WORLD
+   */
+
+  constructor(private xivAPI: XivAPIService, private mbAPI: UniversalisService, private router: Router, private settings: SettingsService) { }
 
   ngOnInit(): void {
+  }
+
+
+  async getData(recipe: Recipe) {
+    const itemName = await this.xivAPI.getName(recipe.item)
+    const homePriceHQ = (await this.mbAPI.getItem(this.settings.homeworld, recipe.item)).minPriceHQ
+
+    const mats = recipe.materialIds
+    const matNames: string[] = []
+
+    for (let i = 0; i < mats.length; i++) {
+
+    }
+    
   }
 
 }
