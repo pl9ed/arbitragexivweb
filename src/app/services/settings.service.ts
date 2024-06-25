d79fbe54a015a240399973a117e4ea4bc940f557
Pai <pl9ed15@gmail.com>
2024-06-25 16:17:51 -0400

    added a few basic configs from yml

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Constants } from '../models/Constants';
+import * as yaml from 'yaml'
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +11,24 @@ export class SettingsService {
 
   homeworld: string = Constants.DEFAULT_HOMEWORLD
 
-  constructor() {}
+  config$: Observable<SettingsConfig>;
+
+  constructor(private http: HttpClient) {
+   this.config$ = this.http.get('assets/config.yml', { responseType: 'text'})
+   .pipe(map((str) => yaml.parse(str)))
+  }
+}
+
+interface SettingsConfig {
+  defaultWorld: string,
+  flip: {
+    dropDownOptions: string[],
+    itemLists: {
+      consumables: number[],
+      craftingGear: number[],
+      craftingMats: number[],
+      materia: number[]
+    }
+  }
+  primal: string[]
 }
