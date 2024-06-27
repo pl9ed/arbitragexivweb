c08e5e3daed3f9ab89ec3255882e387c5f742aa4
Pai <pl9ed15@gmail.com>
2024-06-27 03:55:18 -0400

    display rows based on state

--- src/app/pages/flip/flip.component.ts
+++ src/app/pages/flip/flip.component.ts
@@ -45,8 +45,7 @@ export class FlipComponent implements OnInit {
     'Min Price World (HQ)',
     'Velocity (HQ)',
   ];
-  velocityMap: Map<string, number[]> = new Map<string, number[]>();
-  dataArray: any[] = [];
+
   flipItemIDs = Constants.CONSUMABLE_ITEM_IDS;
 
   selectedItems$!: Observable<number[]>;
@@ -55,9 +54,7 @@ export class FlipComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private mbAPI: UniversalisService,
     private settings: SettingsService,
-    private xivAPI: XivAPIService,
     private store: Store
   ) {}
 
@@ -72,91 +69,21 @@ export class FlipComponent implements OnInit {
     this.rowData$ = this.store.select(selectItemRows)
   }
 
-  fillFlipTable() {
-    this.dataArray = [];
-    let i = 0;
-    const interval = 25 + this.flipItemIDs.length * 40;
-    for (const item of this.flipItemIDs) {
-      setTimeout(async () => {
-        const itemName = (await this.xivAPI.getName(item)).Name;
-
-        const prices = await this.getPrices(
-          { name: itemName, id: item }
-        );
-        const nqPrices = prices[0];
-        const hqPrices = prices[1];
-
-        const nqWorld = [...nqPrices.keys()][0];
-        const hqWorld = [...hqPrices.keys()][0];
-
-        const nqROI =
-          (nqPrices.get(this.settings.homeworld) || 0) /
-          (nqPrices.get(nqWorld) || 0);
-        const hqROI =
-          (hqPrices.get(this.settings.homeworld) || 0) /
-          (hqPrices.get(hqWorld) || 0);
-
-        const nqVelocity: number = (this.velocityMap.get(itemName) || [])[0];
-        const hqVelocity: number = (this.velocityMap.get(itemName) || [])[1];
-
-        const row = [
-          itemName,
-          nqROI.toFixed(2),
-          nqPrices.get(nqWorld),
-          nqPrices.get(this.settings.homeworld),
-          nqWorld,
-          nqVelocity.toFixed(3),
-          hqROI.toFixed(2),
-          hqPrices.get(hqWorld),
-          hqPrices.get(this.settings.homeworld),
-          hqWorld,
-          hqVelocity.toFixed(3),
-        ];
-        this.dataArray.push(row);
-      }, i * interval);
-      i++;
-    }
-  }
-
   togglePrices(index: number) {
     this.flipItemIDs = this.toggleItems[index];
     this.dropdownText = this.dropdownTextOptions[index];
-    this.fillFlipTable();
   }
 
-  async getPrices(
-    item: Item,
-  ): Promise<Map<string, number>[]> {
-    const nqMap = new Map<string, number>();
-    const hqMap = new Map<string, number>();
+  format(num: number | null) {
+    if (num) return num.toFixed(2)
+    return "n/a"
+  }
 
-    for (let i = 0; i < Constants.PRIMAL.length; i++) {
-      const world = Constants.PRIMAL[i];
-      const prices = await this.mbAPI.getItem(world, item.id);
-      nqMap.set(world, prices.minPriceNQ);
-      hqMap.set(world, prices.minPriceHQ);
-      this.velocityMap.set(item.name, [
-        prices.nqSaleVelocity,
-        prices.hqSaleVelocity,
-      ]);
-      await this.mbAPI.sleep(50);
+  emptyIfInvalid(num: number) {
+    if (Number.isNaN(num) || !Number.isFinite(num)) {
+      return null
+    } else {
+      return num
     }
-
-    // remove with no price
-    const filteredNQ = new Map<string, number>(
-      [...nqMap].filter(([k, v]) => v > 0),
-    );
-    const filteredHQ = new Map<string, number>(
-      [...hqMap].filter(([k, v]) => v > 0),
-    );
-
-    const sortedNQ = new Map<string, number>(
-      [...filteredNQ.entries()].sort((a, b) => a[1] - b[1]),
-    );
-    const sortedHQ = new Map<string, number>(
-      [...filteredHQ.entries()].sort((a, b) => a[1] - b[1]),
-    );
-
-    return [sortedNQ, sortedHQ];
   }
 }
