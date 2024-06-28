be8c742e078f5c1cfffbe739c9fd92dddecb53ed
Pai <pl9ed15@gmail.com>
2024-06-27 22:05:55 -0400

    Removed unused constants

--- src/app/models/Constants.ts
+++ src/app/models/Constants.ts
@@ -1,5 +1,3 @@
-import { Region } from './Region';
-
 export abstract class Constants {
   public static PRIMAL = [
     'Behemoth',
@@ -20,36 +18,6 @@ export abstract class Constants {
     38891, 38892, 38893, 38894, 38895, 38896, 38897, 38898, 38899, 38900, 38901,
     38902, 38903, 38904, 38905, 38906, 38907, 38908, 38909, 38910,
   ];
-  public static GEMSTONE_ITEMS_LVL1: GemstoneItem[] = [
-    { name: 'Gaja Hide', id: 36242, area: Region.THAVNAIR, cost: 2 },
-    { name: 'Kumbhira Skin', id: 36245, area: Region.THAVNAIR, cost: 2 },
-    { name: 'Berkanan Sap', id: 36261, area: Region.LABYRINTHOS, cost: 2 },
-    {
-      name: 'Luncheon Toad Skin',
-      id: 36243,
-      area: Region.LABYRINTHOS,
-      cost: 2,
-    },
-    { name: 'Saiga Hide', id: 36244, area: Region.GARLEMALD, cost: 2 },
-    { name: 'Almasty Fur', id: 36203, area: Region.GARLEMALD, cost: 2 },
-    { name: 'Mousse Flesh', id: 36257, area: Region.MARE_LAMENTORUM, cost: 2 },
-    {
-      name: 'Lunatender Blossom',
-      id: 36258,
-      area: Region.MARE_LAMENTORUM,
-      cost: 2,
-    },
-    { name: 'Dynamite Ash', id: 36259, area: Region.MARE_LAMENTORUM, cost: 2 },
-    { name: 'Petalouda Scales', id: 36260, area: Region.ELPIS, cost: 2 },
-    { name: 'Ophiotauros Hide', id: 36246, area: Region.ELPIS, cost: 2 },
-    { name: 'Dynamis Crystal', id: 36262, area: Region.ULTIMA_THULE, cost: 2 },
-    {
-      name: 'Bicolor Gemstone Voucher',
-      id: 35833,
-      area: Region.OLD_SHARLAYAN,
-      cost: 100,
-    },
-  ];
 
   public static MATERIA: number[] = [
     33919, 33932, 33918, 33931, 33920, 33933, 33929, 33942, 33928, 33941,
@@ -62,9 +30,3 @@ export abstract class Constants {
   public static DEFAULT_PRICECHECK_ITEMS: number[] = Constants.TOME_MATS;
 }
 
-export interface GemstoneItem {
-  name: string;
-  id: number;
-  area: Region;
-  cost: number;
-}
