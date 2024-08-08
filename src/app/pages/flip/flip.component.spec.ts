7b13e1a12c7169c75bb15ce991718ad1b009a658
Pai <pl9ed15@gmail.com>
2024-08-08 04:41:04 -0400

    Updated component name for flip unit tests

--- src/app/pages/flip/flip.component.spec.ts
+++ src/app/pages/flip/flip.component.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
+import { FlipComponent } from './flip.component';
 
 describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+  let component: FlipComponent;
+  let fixture: ComponentFixture<FlipComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [HomeComponent],
+      declarations: [FlipComponent],
     }).compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
+    fixture = TestBed.createComponent(FlipComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
