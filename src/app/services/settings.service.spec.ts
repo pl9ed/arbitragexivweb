5e829c69a9eddc15b5fcf302a13b2361f42eb4d0
Pai <pl9ed15@gmail.com>
2024-08-08 05:34:13 -0400

    Added unit tests for settings service

--- src/app/services/settings.service.spec.ts
+++ src/app/services/settings.service.spec.ts
@@ -1,16 +1,72 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 import { SettingsService } from './settings.service';
+import { Constants } from '../models/Constants';
+import { SettingsConfig } from './settings.models';
+import * as yaml from 'yaml';
+import { provideHttpClient } from '@angular/common/http';
 
-describe('SettingsService', () => {
+fdescribe('SettingsService', () => {
   let service: SettingsService;
+  let httpMock: HttpTestingController;
+
+  const mockConfig: SettingsConfig = { 
+        homeworld: "homeworld",
+        primal: ['primal1', 'primal2', 'primal3'],
+        flip: {
+          dropDownOptions: ['option1', 'option2', 'option3'],
+          itemLists: {
+            consumables: [1, 2, 3],
+            craftingGear: [4, 5, 6],
+            craftingMats: [7, 8, 9],
+            materia: [10, 11, 12],
+          },
+        },
+        gemstoneItems: [
+          {
+          name: "gemstone",
+          id: 123,
+          area: "area",
+          cost: 2,
+        },
+        ],
+        pricechecker: {
+          default: [1, 2, 3, 4, 5],
+        }
+    };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [SettingsService, provideHttpClient(), provideHttpClientTesting()]
+    });
+
     service = TestBed.inject(SettingsService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should have default homeworld', () => {
+    expect(service.homeworld).toBe(Constants.DEFAULT_HOMEWORLD);
+  });
+
+  it('should fetch and parse settings config', (done) => {
+    
+    const mockYaml = yaml.stringify(mockConfig);
+
+    service.settingsConfig$.subscribe((config: SettingsConfig) => {
+      expect(config).toEqual(mockConfig);
+      done();
+    });
+
+    const req = httpMock.expectOne('assets/config.yml');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockYaml);
   });
 });
